8394181200176f72ce4bf6dd80fc81e96ee59909
islomjon <turgunovislomjon107@gmail.com>
2022-08-02 17:31:03 +0300

    added task2questions  model

--- ielts/models.py
+++ ielts/models.py
@@ -29,3 +29,15 @@ class writingtask1(models.Model):
     score = models.IntegerField(null=True, blank=True)
     type_task = models.CharField(
         max_length=250, choices=TYPES, default="none")
+
+
+class Task2question(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        'Category', related_name='category', default="coding", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.title)
+
+    def get_absolute_url(self):
+        return reverse('home')
