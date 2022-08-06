1d93c70e4b4d101d669be968d24f315b56f92b4d
islomjon <turgunovislomjon107@gmail.com>
2022-08-06 17:28:23 +0300

    add task1 categorielist to admin panel

--- ielts/admin.py
+++ ielts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import writingtask2, writingtask1, Task2question, Category
+from .models import writingtask2, writingtask1, Task2question, Category, CategoryTask1
 # Register your models here.
 
 
@@ -7,3 +7,4 @@ admin.site.register(Category)
 admin.site.register(writingtask1)
 admin.site.register(writingtask2)
 admin.site.register(Task2question)
+admin.site.register(CategoryTask1)
