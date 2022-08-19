c716782d1bd96828cadffbe241d94e5462400bcf
Turgunov Islomjon <89976631+islomjon17@users.noreply.github.com>
2022-08-19 14:38:45 +0300

    test

--- ielts/models.py
+++ ielts/models.py
@@ -64,3 +64,18 @@ class writingtask2(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+
+
+class writingtasksssss (models.Model):
+    question = models.ForeignKey(
+        'Task2question',  default="coding", on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    text = RichTextField(blank=True, null=True)
+    score = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return (self.title)
+
+    def get_absolute_url(self):
+        return reverse('home')
