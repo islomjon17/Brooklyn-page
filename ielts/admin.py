8adfd3f5a713ab48cd6e397bb4f2d0b30ff6e431
Turgunov Islomjon <89976631+islomjon17@users.noreply.github.com>
2022-08-25 08:25:32 +0300

    speaking part 3 model

--- ielts/admin.py
+++ ielts/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import writingtask2, writingtask1, Task2question, CategoryTask2, CategoryTask1, speakingpart1, SpeakingPartOneTopic
-
+from .models import *
 
 admin.site.register(CategoryTask2)
+
 admin.site.register(writingtask1)
+
 admin.site.register(writingtask2)
+
 admin.site.register(Task2question)
+
 admin.site.register(CategoryTask1)
+
 admin.site.register(speakingpart1)
+
 admin.site.register(SpeakingPartOneTopic)
+
+admin.site.register(SpeakingPart2)
+
+admin.site.register(SpeakingPartThreeTopic)
+
+admin.site.register(speakingpart3)
+
+
+
+
--- ielts/models.py
+++ ielts/models.py
@@ -118,14 +118,59 @@ class speakingpart1(models.Model):
     
     question8 = models.CharField(max_length=255, blank=True)
     answer8 = models.TextField(blank=True)
-    
+ 
+ 
+    def __str__(self) -> str:
+        return self.topic    
+       
     
     
 class SpeakingPart2(models.Model):
     question = models.CharField(max_length=255)
     text = RichTextField(blank=True, null=True)
 
+    def __str__(self) -> str:
+        return self.question    
     
     
+class SpeakingPartThreeTopic(models.Model):
+    title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return (self.title)
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
+class speakingpart3(models.Model):
+    topic = models.ForeignKey(
+        'SpeakingPartThreeTopic',  default="unnamed", on_delete=models.CASCADE)
     
+    question1 = models.CharField(max_length=255)
+    answer1 = models.TextField()
+    
+    question2 = models.CharField(max_length=255, blank=True)
+    answer2 = models.TextField(blank=True)
+    
+    question3 = models.CharField(max_length=255, blank=True)
+    answer3 = models.TextField(blank=True)
+    
+    question4 = models.CharField(max_length=255, blank=True)
+    answer4 = models.TextField(blank=True)
+    
+    question5 = models.CharField(max_length=255, blank=True)
+    answer5 = models.TextField(blank=True)
+    
+    question6 = models.CharField(max_length=255, blank=True)
+    answer6 = models.TextField(blank=True)
+    
+    question7 = models.CharField(max_length=255, blank=True)
+    answer7 = models.TextField(blank=True)
+    
+    question8 = models.CharField(max_length=255, blank=True)
+    answer8 = models.TextField(blank=True)
+ 
+ 
+    def __str__(self) -> str:
+        return self.topic    
