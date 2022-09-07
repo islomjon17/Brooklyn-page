8675be7c1768ffc25a38030ca1beea39ed49d3c3
Turgunov Islomjon <89976631+islomjon17@users.noreply.github.com>
2022-09-07 08:14:37 +0300

    fix vocabulary model return object

--- ielts/models.py
+++ ielts/models.py
@@ -155,6 +155,9 @@ class Vocabulary(models.Model):
     word = models.CharField(max_length=100)
     description = models.TextField()
     example = models.TextField()
+    
+    def __str__(self):
+        return self.word
 
  
  
